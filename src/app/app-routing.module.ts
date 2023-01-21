adf3ef38f7ac176ff3b74df6e7a64512b3cf55b0
Rafael <rafaelsr84@gmail.com>
2023-01-21 00:59:16 -0300

    deploy final en Firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
 { path:'', component: HomeComponent},
-{ path:'login', component:LoginComponent},
+{ path: 'login', component:LoginComponent},
 { path: 'nuevaexp', component: NewExperienciaComponent},
 { path: 'editexp/:id', component: EditExperienciaComponent},
 { path: 'nuevaedu', component: NeweducacionComponent},
